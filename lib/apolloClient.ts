df68aabf3a1f5b0cf5bc77754305c887c0b87ba3
Phillip Foo <pfoo360@gmail.com>
2023-02-28 19:51:15 -0500

    updated apollo client uri

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const uri = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
-  : "http://localhost:3000/api/graphql";
+const uri =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000/api/graphql"
+    : `https://${process.env.NEXT_PUBLIC_SITE_URL}/api/graphql`;
 
 const client = new ApolloClient({
   uri,
